89cf387a9e4657818a58fb36c6cc6d091c14a5e4
Olzhasuh <241407@astanait.edu.kz>
2026-02-07 23:44:32 +0500

    Olzhas: add basic campaign creation logic

--- contracts/StartupBooster.sol
+++ contracts/StartupBooster.sol
@@ -2,5 +2,27 @@
 pragma solidity ^0.8.20;
 
 contract StartupBooster {
-    
+    struct Campaign {
+        address creator;
+        string title;
+        uint256 goal;
+        uint256 deadline;
+    }
+
+    uint256 public campaignCount;
+    mapping(uint256 => Campaign) public campaigns;
+
+    function createCampaign(string calldata title, uint256 goal, uint256 duration) external {
+        require(goal > 0);
+        require(duration > 0);
+
+        campaignCount++;
+
+        campaigns[campaignCount] = Campaign(
+            msg.sender,
+            title,
+            goal,
+            block.timestamp + duration
+        );
+    }
 }
