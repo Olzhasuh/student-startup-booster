e6c8864f1e4a6a8f71deafe38e1a172b3d43c929
Dias <nygmanovdias28@gmail.com>
2026-02-08 13:40:49 +0500

    Dias: add mint to SupportToken

--- contracts/SupportToken.sol
+++ contracts/SupportToken.sol
@@ -5,5 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SupportToken is ERC20, Ownable {
+    mapping(address => bool) public minters;
+
     constructor() ERC20("SupportToken", "SUP") Ownable(msg.sender) {}
+
+    function setMinter(address minter, bool enabled) external onlyOwner {
+        minters[minter] = enabled;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(minters[msg.sender], "Not minter");
+        _mint(to, amount);
+    }
 }
