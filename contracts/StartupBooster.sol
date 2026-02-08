4ecf602cf3de6e3d80d7dd48484b3f7786895417
Alisher Sultanov <dragons55u@gmail.com>
2026-02-08 13:46:11 +0500

    feat: frontend MetaMask connect + UI + favicon

--- contracts/StartupBooster.sol
+++ contracts/StartupBooster.sol
@@ -1,20 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+interface ISupportToken {
+    function mint(address to, uint256 amount) external;
+}
+
 contract StartupBooster {
     struct Campaign {
         address creator;
         string title;
         uint256 goal;
         uint256 deadline;
+        uint256 pledged;
     }
+
+    ISupportToken public token;
 
     uint256 public campaignCount;
     mapping(uint256 => Campaign) public campaigns;
 
-    function createCampaign(string calldata title, uint256 goal, uint256 duration) external {
-        require(goal > 0);
-        require(duration > 0);
+    constructor(address tokenAddress) {
+        token = ISupportToken(tokenAddress);
+    }
+
+    function createCampaign(
+        string calldata title,
+        uint256 goal,
+        uint256 duration
+    ) external {
+        require(goal > 0, "Goal must be > 0");
+        require(duration > 0, "Duration must be > 0");
 
         campaignCount++;
 
@@ -22,7 +37,8 @@ contract StartupBooster {
             msg.sender,
             title,
             goal,
-            block.timestamp + duration
+            block.timestamp + duration,
+            0
         );
     }
 }
--- contracts/SupportToken.sol
+++ contracts/SupportToken.sol
@@ -5,5 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SupportToken is ERC20, Ownable {
+    mapping(address => bool) public minters;
+
     constructor() ERC20("SupportToken", "SUP") Ownable(msg.sender) {}
+
+    function setMinter(address minter, bool allowed) external onlyOwner {
+        minters[minter] = allowed;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(minters[msg.sender], "Not a minter");
+        _mint(to, amount);
+    }
 }
